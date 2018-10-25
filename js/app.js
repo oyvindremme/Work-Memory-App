846570a42180b499d7726b5b16ca86494b258c40
Øyvind Remme <oyvind.remme@gmail.com>
2018-10-25 02:08:45 +0200

    added auto indentation for focusing fields

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-const app = document.getElementById('app');
+const numberContainer = document.getElementById('numbers');
 const appNumbers = document.getElementsByClassName('application__number');
 
 let numbers = [];
-let guesses = [];
+let userGuesses = [];
 
 const functions = {
     init: () => {
-        app.classList.add('flex-column');
+        numberContainer.classList.add('flex-column');
         numbers = [];
         guesses = [];
-        app.innerHTML = `
+        numberContainer.innerHTML = `
             <h1>Hello!</h1>
             <p class="lead">Press the start button to begin.</p>
             <button class="btn btn-lg btn-success" onclick="functions.showNumbers()">Start</button>
         `;
     },
     showNumbers: () => {
-        app.classList.remove('flex-column');
-        app.innerHTML = ``;
+        numberContainer.classList.remove('flex-column');
+        numberContainer.innerHTML = ``;
         while (numbers.length < 4) {
             numbers.push(Math.floor(Math.random() * 10));
         }
         numbers.forEach(number => {
-            app.innerHTML += `
+            numberContainer.innerHTML += `
                 <div class="col-3">
                     <input type="text" class="application__number" value="${number}" maxlength="1" readonly>
                 </div>
@@ -33,15 +33,25 @@ const functions = {
         }, 2000);
     },
     guessNumbers: () => {
-        app.innerHTML = ``;
+        numberContainer.innerHTML = ``;
         numbers.forEach(() => {
-            app.innerHTML += `
+            numberContainer.innerHTML += `
                 <div class="col-3">
-                    <input type="number" class="application__number" inputMode="numeric" pattern="[0-9]*" maxlength="1">
+                    <input type="text" class="application__number" inputMode="numeric" pattern="[0-9]*" maxlength="1">
                 </div>
             `;
         });
         appNumbers[0].focus();
+        console.log(appNumbers.length);
+        for (let i = 0; i < numbers.length; i++) {
+            appNumbers[i].addEventListener("keyup", () => {
+                if (i+1 == appNumbers.length) {
+                    functions.getScore();
+                } else {
+                    appNumbers[i+1].focus();
+                }
+            });
+        }
         setTimeout(() => {
             for (let i = 0; i < numbers.length; i++) {
                 typeof parseInt(appNumbers[i].value) === 'number' ? guesses.push(parseInt(appNumbers[i].value)) : guesses.push('?');
@@ -51,23 +61,23 @@ const functions = {
 
     },
     getScore: () => {
-        app.innerHTML = ``;
+        numberContainer.innerHTML = ``;
         for (let i = 0; i < guesses.length; i++) {
             if (numbers[i] === guesses[i]) {
-                app.innerHTML += `
+                numberContainer.innerHTML += `
                     <div class="col-3">
                         <input id="inp${i + 1}" type="text" class="application__number border-success text-success" value="${guesses[i]}" maxlength="1" readonly>
                     </div>
                 `;
             } else {
-                app.innerHTML += `
+                numberContainer.innerHTML += `
                     <div class="col-3">
                         <input id="inp${i + 1}" type="text" class="application__number border-danger text-danger" value="${guesses[i]}" maxlength="1" readonly>
                     </div>
                 `;
             }
         }
-        app.innerHTML += `
+        numberContainer.innerHTML += `
             <button class="btn btn-lg btn-success mt-5" onclick="functions.init()">Try again</button>
         `;
     }
