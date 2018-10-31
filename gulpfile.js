2bfcc78b8f99ae134f57fe4dcecbdf96ff7a183d
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2018-10-31 19:58:15 +0100

    added gulp sass pipe to project

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 
+sass.compiler = require('node-sass');
+
 gulp.task('babel',  () => {
     gulp.src('js/app.js')
         .pipe(babel({
@@ -10,6 +12,12 @@ gulp.task('babel',  () => {
         .pipe(gulp.dest('public/js'))
 });
 
+gulp.task('sass', () => {
+    gulp.src('sass/style.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('public/css'));
+  });
+
 gulp.task('watch', () => {
-    gulp.watch('js/app.js', [ 'babel' ]);
+    gulp.watch(['js/app.js', 'sass/style.scss'], [ 'babel', 'sass' ]);
 });
