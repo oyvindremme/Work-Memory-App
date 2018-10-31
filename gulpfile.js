f30033ff5c437dedbf1699529eb28ae76eafc25b
LAPTOP-DVJFUS5D\oyvin <oyvind.remme@gmail.com>
2018-10-31 23:16:38 +0100

    updated gulp autoprefixer pipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 sass.compiler = require('node-sass');
 
@@ -15,8 +16,12 @@ gulp.task('babel',  () => {
 gulp.task('sass', () => {
     gulp.src('sass/style.scss')
       .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+        }))
       .pipe(gulp.dest('public/css'));
-  });
+});
 
 gulp.task('watch', () => {
     gulp.watch(['js/app.js', 'sass/style.scss'], [ 'babel', 'sass' ]);
